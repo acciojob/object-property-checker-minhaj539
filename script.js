bc97743fa00b273cd77c9a56792b37202eaeddc4
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-04-15 02:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
-}
+	if(sampleObject.key!=undefined) {
+		console.log("true");
+	}
+	 else {
+		 console.log("false") ;
+	 }
+	
 
 // Do not change the code below
 const key = prompt("Enter Key.");
